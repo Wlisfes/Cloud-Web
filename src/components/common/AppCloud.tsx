42e9818346cfa4b2364ce2f245fe5001c736d302
fanyanghua <876451336@qq.com>
2021-08-05 22:37:59 +0800

    完善媒体表单

--- src/components/common/AppCloud.tsx
+++ src/components/common/AppCloud.tsx
@@ -3,7 +3,7 @@ import { FormModel, Input, Modal, Button, Upload, Spin } from 'ant-design-vue'
 
 @Component
 export default class AppCloud extends Vue {
-	$refs!: { form: FormModel }
+	$refs!: { formModel: FormModel }
 
 	private visible: boolean = false
 	private loading: boolean = false
@@ -12,19 +12,24 @@ export default class AppCloud extends Vue {
 		wrapperCol: { span: 20, style: { width: 'calc(100% - 100px)' } },
 		form: {
 			title: '',
-			file: null
+			file: null,
+			name: ''
 		},
 		rules: {
-			name: [{ required: true, message: '请输入分类标签名称', trigger: 'blur' }],
-			color: [{ required: true, message: '请选择分类标签颜色', trigger: 'blur' }]
+			title: [{ required: true, message: '请输入媒体名称', trigger: 'blur' }],
+			file: [{ required: true, message: '请上次媒体文件', trigger: 'blur' }]
 		}
 	}
 
 	private beforeUpload(file: File) {
-		console.log(file)
+		this.state.form = Object.assign(this.state.form, {
+			file,
+			name: file.name
+		})
 		return false
 	}
 
+	/**组件调用**/
 	public init() {
 		this.visible = true
 	}
@@ -34,13 +39,20 @@ export default class AppCloud extends Vue {
 		this.visible = false
 		setTimeout(() => {
 			this.loading = false
+			this.state.form = Object.assign(this.state.form, {
+				title: '',
+				file: null,
+				name: ''
+			})
 		}, 300)
 	}
 
 	/**组件提交事件**/
 	private onSubmit() {
-		this.$refs.form.validate(valid => {
+		this.$refs.formModel.validate(valid => {
 			if (valid) {
+				this.$emit('submit', { ...this.state.form })
+				this.onClose()
 			}
 		})
 	}
@@ -57,17 +69,26 @@ export default class AppCloud extends Vue {
 				onCancel={this.onClose}
 			>
 				<Spin size="large" spinning={this.loading}>
-					<FormModel
-						ref="form"
-						layout="horizontal"
-						{...{ props: { model: form, rules } }}
-						labelCol={labelCol}
-						wrapperCol={wrapperCol}
-					>
-						<FormModel.Item label="媒体标题" prop="title">
+					<FormModel ref="formModel" class="app-form" {...{ props: { model: form, rules } }}>
+						<FormModel.Item wrapperCol={wrapperCol} labelCol={labelCol} label="媒体标题" prop="title">
 							<Input v-model={form.title} allowClear placeholder="媒体标题"></Input>
 						</FormModel.Item>
-						<FormModel.Item label="媒体标题" prop="file"></FormModel.Item>
+						<FormModel.Item wrapperCol={wrapperCol} labelCol={labelCol} label="媒体文件" prop="file">
+							<div style={{ display: 'flex' }}>
+								<Upload
+									accept=".mp4,.flv,.mkv,.mov"
+									beforeUpload={this.beforeUpload}
+									showUploadList={false}
+								>
+									<Button type="primary" icon="upload" disabled={this.loading}>
+										选择文件
+									</Button>
+								</Upload>
+								<div class="app-ellipsis" style={{ flex: 1, marginLeft: '10px', cursor: 'pointer' }}>
+									{form.name}
+								</div>
+							</div>
+						</FormModel.Item>
 					</FormModel>
 				</Spin>
 				<div slot="footer" style={{ display: 'flex', justifyContent: 'center' }}>
