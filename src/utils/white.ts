324a94d5ac17e88c4e17ac4d2aaebafd58597f30
lifei <876451336@qq.com>
2021-08-16 10:24:11 +0800

    添加前台白名单页面

--- src/utils/white.ts
+++ src/utils/white.ts
@@ -1,6 +1,15 @@
-const whiteList: string[] = ['/main/login', '/main/register', '/404']
+const whiteList: string[] = [
+	'/', //首页
+	'/multiple', //归档
+	'/intense', //视频
+	'/minute', //笔记
+	'/partner', //友链
+	'/main/login', //登录
+	'/main/register', //注册
+	'/404' //404
+]
 
 /**路由白名单验证**/
 export function isWhite(path: string): boolean {
-	return whiteList.some(k => path.indexOf(k) !== -1)
+	return whiteList.indexOf(path) !== -1
 }
