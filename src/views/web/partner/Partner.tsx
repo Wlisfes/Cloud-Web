7fbea34202315dd92b1550fb3b0e8af1119126cf
lifei <876451336@qq.com>
2021-08-31 18:06:14 +0800

    新增生活时间轴展示

--- src/views/web/partner/Partner.tsx
+++ src/views/web/partner/Partner.tsx
@@ -1,8 +1,14 @@
 import { Vue, Component } from 'vue-property-decorator'
+import { NodePartner } from '@/views/web/partner/common'
+import style from '@/style/web/web.partner.module.less'
 
 @Component
 export default class Partner extends Vue {
 	protected render() {
-		return <div>Partner</div>
+		return (
+			<div class={style['app-conter']}>
+				<NodePartner></NodePartner>
+			</div>
+		)
 	}
 }
---
+++ src/views/web/partner/common/NodePartner.tsx
@@ -0,0 +1,33 @@
+import { Vue, Component } from 'vue-property-decorator'
+import { Timeline, TimelineItem, Card } from 'element-ui'
+import style from '@/style/web/common/node.partner.module.less'
+
+@Component
+export default class NodePartner extends Vue {
+	protected render() {
+		return (
+			<div class={style['app-conter']}>
+				<Timeline>
+					<TimelineItem timestamp="2018/4/14" hide-timestamp>
+						<Card>
+							<h4>更新 Github 模板</h4>
+							<p>王小虎 提交于 2018/4/12 20:46</p>
+						</Card>
+					</TimelineItem>
+					<TimelineItem timestamp="2018/4/13" hide-timestamp>
+						<Card>
+							<h4>更新 Github 模板</h4>
+							<p>王小虎 提交于 2018/4/12 20:46</p>
+						</Card>
+					</TimelineItem>
+					<TimelineItem timestamp="2018/4/12" hide-timestamp>
+						<Card>
+							<h4>初次发布v1.0</h4>
+							<p>王小虎 提交于 2018/4/12 20:46</p>
+						</Card>
+					</TimelineItem>
+				</Timeline>
+			</div>
+		)
+	}
+}
