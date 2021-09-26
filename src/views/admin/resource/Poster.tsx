672e94d43046882299771cb96aed9a22dc77ad9c
fanyanghua <876451336@qq.com>
2021-09-26 23:09:09 +0800

    修改图床列表

--- src/views/admin/resource/Poster.tsx
+++ src/views/admin/resource/Poster.tsx
@@ -1,7 +1,7 @@
 import { Vue, Component } from 'vue-property-decorator'
-import { Button, FormModel, Select, Spin, notification } from 'ant-design-vue'
+import { Button, FormModel, Select, Tag, Table, notification } from 'ant-design-vue'
 import { Image } from 'element-ui'
-import { AppRootNode } from '@/components/common'
+import { AppRootNode, AppCutover, AppSatus } from '@/components/common'
 import { nodePosters } from '@/api'
 import { HttpStatus, Source, NodePosterNodeResponse } from '@/types'
 import style from '@/style/admin/admin.poster.module.less'
@@ -16,11 +16,19 @@ type SourceOption = {
 @Component
 export default class Poster extends Vue {
 	private source: Source<Array<NodePosterNodeResponse>> & SourceOption = {
-		column: [],
+		column: [
+			{ title: '图片预览', align: 'center', width: 150, scopedSlots: { customRender: 'cover' } },
+			{ title: '图片类型', align: 'center', width: 150, scopedSlots: { customRender: 'type' } },
+			{ title: '图片地址', align: 'center', width: '12%', scopedSlots: { customRender: 'path' } },
+			{ title: '图片备注', dataIndex: 'comment', align: 'center' },
+			{ title: '图片状态', align: 'center', width: '12%', scopedSlots: { customRender: 'status' } },
+			{ title: '创建时间', dataIndex: 'createTime', align: 'center', width: '20%' },
+			{ title: '操作', align: 'center', width: '12%', scopedSlots: { customRender: 'action' } }
+		],
 		page: 1,
 		size: 10,
 		total: 0,
-		sizeOption: ['5', '10', '15', '20', '30', '40', '50'],
+		sizeOption: ['10', '15', '20', '30', '40', '50'],
 		showSize: true,
 		loading: true,
 		dataSource: [],
@@ -118,13 +126,61 @@ export default class Poster extends Vue {
 						</FormModel.Item>
 					</FormModel>
 
-					<Spin class={`ant-spin-64 ${style['app-spin']}`} spinning={source.loading}>
-						<div class={style['node-conter']}>
-							{source.dataSource.map(k => (
-								<div class={style['node-conter-item']}></div>
-							))}
-						</div>
-					</Spin>
+					<Table
+						class="app-source is-title"
+						bordered
+						rowKey={(record: any) => record.id}
+						loading={{ wrapperClassName: 'ant-spin-64', spinning: source.loading }}
+						columns={source.column}
+						dataSource={source.dataSource}
+						scroll={{ x: 840 }}
+						pagination={{
+							pageSize: source.size,
+							current: source.page,
+							pageSizeOptions: source.sizeOption,
+							showSizeChanger: source.showSize,
+							total: source.total
+						}}
+						onChange={source.onChange}
+						{...{
+							scopedSlots: {
+								cover: (props: NodePosterNodeResponse) => (
+									<div class={style['app-conter-cover']}>
+										<Image
+											fit="cover"
+											src={`${props.url}?x-oss-process=style/resize-16-9`}
+											style={{ width: '96px', height: '54px', cursor: 'pointer' }}
+										></Image>
+									</div>
+								),
+								path: (props: NodePosterNodeResponse) => (
+									<a target="_blank" href={props.url}>
+										<Tag color="purple">大图预览</Tag>
+									</a>
+								),
+								type: (props: NodePosterNodeResponse) => {
+									return props.type === 1 ? (
+										<Tag color="pink">Avatar</Tag>
+									) : props.type === 2 ? (
+										<Tag color="cyan">Upload</Tag>
+									) : (
+										<Tag color="blue">Cover</Tag>
+									)
+								},
+								status: (props: NodePosterNodeResponse) => <AppSatus status={props.status}></AppSatus>,
+								action: (props: NodePosterNodeResponse) => (
+									<Button.Group>
+										<Button type="link">
+											<AppCutover status={props.status}></AppCutover>
+										</Button>
+										<Button type="link">
+											<span style={{ color: '#ff4d4f' }}>删除</span>
+										</Button>
+									</Button.Group>
+								)
+							}
+						}}
+					></Table>
 				</div>
 			</AppRootNode>
 		)
