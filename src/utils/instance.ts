9d34e6e34015f0f7e343aff2894018519cbdf5c5
lifei <876451336@qq.com>
2021-12-07 13:45:23 +0800

    新增实例操作

---
+++ src/utils/instance.ts
@@ -0,0 +1,27 @@
+export function useInstance() {
+	return {
+		onMounte: (callback: Function) => {
+			return new Promise(resolve => {
+				typeof callback === 'function' && callback()
+				resolve('mounte')
+			})
+		},
+		onUnmounte: ({ el, callback, remove }: { el: HTMLElement; callback?: Function; remove?: boolean }) => {
+			return new Promise((resolve, reject) => {
+				if (!el) {
+					reject('el元素不能为空')
+				}
+
+				typeof callback === 'function' && callback()
+				const timeout = setTimeout(() => {
+					if (remove) {
+						el?.previousElementSibling?.remove?.()
+						el?.remove?.()
+						clearTimeout(timeout)
+					}
+					resolve('unMounte')
+				}, 300)
+			})
+		}
+	}
+}
