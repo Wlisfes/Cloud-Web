7ef8d92e1a31170bda26c5c265704f7063c4cd18
lifei <876451336@qq.com>
2022-01-07 15:57:33 +0800

    新增评论相关接口

---
+++ src/api/comment.service.ts
@@ -0,0 +1,38 @@
+import request from '@/utils/request'
+import * as types from '@/types'
+
+/**创建评论**/
+export function nodeCreateComment(params: types.NodeCreateCommentParameter) {
+	return request<types.NodeCreateCommentResponse>({
+		url: `/api/comment/create`,
+		method: 'POST',
+		data: params
+	})
+}
+
+/**切换评论状态**/
+export function nodeCommentCutover(params: types.NodeCommentCutoverParameter) {
+	return request<types.NodeCommentCutoverResponse>({
+		url: `/api/comment/cutover`,
+		method: 'PUT',
+		data: params
+	})
+}
+
+/**评论列表**/
+export function nodeComments(params: types.NodeCommentsParameter) {
+	return request<types.NodeCommentsResponse>({
+		url: `/api/comment/list-node`,
+		method: 'GET',
+		params
+	})
+}
+
+/**删除评论**/
+export function nodeDeleteComment(params: types.NodeDeleteCommentParameter) {
+	return request<types.NodeDeleteCommentResponse>({
+		url: `/api/comment/del`,
+		method: 'DELETE',
+		params
+	})
+}
---
+++ src/types/comment.types.ts
@@ -0,0 +1,51 @@
+import { NodeDate, NodeUserResponse } from '@/types'
+
+export type NodeComment = NodeDate & {
+	id: number
+	one: number
+	super: number
+	type: number
+	comment: string
+	status: number
+	parent: NodeComment
+	user: NodeUserResponse
+	reply: {
+		total: number
+		page: number
+		size: number
+		list: NodeComment[]
+	}
+}
+export type NodeCommentParameter = NodeComment & {
+	total: number
+	page: number
+	size: number
+	message: string
+}
+
+/**
+ * 创建评论
+ */
+export type NodeCreateCommentParameter = Pick<NodeCommentParameter, 'one' | 'type' | 'comment' | 'super' | 'parent'>
+export type NodeCreateCommentResponse = Pick<NodeCommentParameter, 'message'>
+
+/**
+ * 切换评论状态
+ */
+export type NodeCommentCutoverParameter = Pick<NodeCommentParameter, 'id'>
+export type NodeCommentCutoverResponse = Pick<NodeCommentParameter, 'message'>
+
+/**
+ * 评论列表
+ */
+export type NodeCommentsParameter = Pick<NodeCommentParameter, 'one' | 'type' | 'page' | 'size'> &
+	Partial<Pick<NodeCommentParameter, 'super'>>
+export type NodeCommentsResponse = Pick<NodeCommentParameter, 'page' | 'size' | 'total'> & {
+	list: NodeComment[]
+}
+
+/**
+ * 删除评论
+ */
+export type NodeDeleteCommentParameter = Pick<NodeCommentParameter, 'id'>
+export type NodeDeleteCommentResponse = Pick<NodeCommentParameter, 'message'>
