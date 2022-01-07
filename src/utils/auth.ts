93e2d4469349b6318407b195aad1d532bb843527
lifei <876451336@qq.com>
2022-01-07 18:14:49 +0800

    添加验证函数

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie'
 const APP_AUTH_TOKEN = Symbol('APP_AUTH_TOKEN').toString()
+import { init as initMain } from '@/components/instance/init-main'
 
 export function getToken(): string | undefined {
 	return Cookies.get(APP_AUTH_TOKEN)
@@ -11,4 +12,33 @@ export function setToken(token: string) {
 
 export function delToken() {
 	return Cookies.remove(APP_AUTH_TOKEN)
+}
+
+/**验证是否登录**/
+export function stopAuth(fn?: Function) {
+	if (getToken()) {
+		typeof fn === 'function' && fn()
+	} else {
+		initMain().then(node => {
+			node.$once('close', (done: Function) => done())
+			node.$once('submit', (done: Function) => {
+				typeof fn === 'function' && fn()
+				done()
+			})
+		})
+	}
+}
+
+/**阻止默认事件**/
+export function stopEvent(e: Event, fn?: Function) {
+	e.preventDefault()
+	e.stopPropagation()
+	typeof fn === 'function' && fn()
+}
+
+/**阻止默认事件 -> 登录验证**/
+export function stopBack(e: Event, fn?: Function) {
+	stopEvent(e, () => {
+		stopAuth(fn)
+	})
 }
